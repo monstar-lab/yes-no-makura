8142aacf35f1cbcec5b68ffbe10f1aab9348dde9
t-kazu <s153057@matsu.shimane-u.ac.jp>
2017-02-23 12:52:18 +0900

    全画面にすると選択できないバグを解消

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -17,6 +17,7 @@ Rails.application.config.assets.precompile += %w( style.css )
 Rails.application.config.assets.precompile += %w( fonts.css )
 Rails.application.config.assets.precompile += %w( span.css )
 Rails.application.config.assets.precompile += %w( frame.css )
+Rails.application.config.assets.precompile += %w( priority.css )
 
 Rails.application.config.assets.precompile += %w( modernizr-2.6.2.min.js )
 Rails.application.config.assets.precompile += %w( jquery.min.js )
