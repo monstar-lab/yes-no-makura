36e005c7cc907de9930b0c17fb961867d514736e
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2017-02-24 22:56:57 +0900

    :recycle: Remove unicorn

--- config/unicorn.rb
+++
@@ -1,9 +0,0 @@
-worker_processes 2
-
-listen File.expand_path('tmp/sockets/unicorn.sock')
-pid    File.expand_path('tmp/pids/unicorn.pid')
-
-stderr_path File.expand_path('log/unicorn.log')
-stdout_path File.expand_path('log/unicorn.log')
-
-preload_app true
