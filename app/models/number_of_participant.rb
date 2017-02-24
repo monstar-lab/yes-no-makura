70b45521654d81b30b5bf914bc43e16168789163
R-adachi <s153002@matsu.shimane-u.ac.jp>
2017-02-24 16:52:36 +0900

    make_number_of_participant_validate

--- app/models/number_of_participant.rb
+++ app/models/number_of_participant.rb
@@ -1,2 +1,8 @@
-class NumberOfParticipant < ApplicationRecord
+class NumberOfParticipant < ActiveRecord::Base
+  validate :number_of_participant_too_many_numbers, on: :create
+  def number_of_participant_too_many_numbers
+    if NumberOfParticipant.all.count > 0
+      errors[:base] << "too_many_numbers"
+    end
+  end
 end
