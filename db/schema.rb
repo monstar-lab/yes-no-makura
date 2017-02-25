f061a266c1f77fc42648abca0e148fa3dce552a7
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2017-02-25 10:48:12 +0900

    :shell: rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,25 +11,27 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20161012020637) do
-  create_table 'answers', force: :cascade do |t|
-    t.integer  'question_id'
-    t.boolean  'yes'
-    t.string   'cookie_key'
-    t.datetime 'created_at',  null: false
-    t.datetime 'updated_at',  null: false
-    t.index ['question_id'], name: 'index_answers_on_question_id'
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "question_id"
+    t.boolean  "yes"
+    t.string   "cookie_key"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
-  create_table 'number_of_participants', force: :cascade do |t|
-    t.integer  'number'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "number_of_participants", force: :cascade do |t|
+    t.integer  "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.string   'body'
-    t.string   'state'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "questions", force: :cascade do |t|
+    t.string   "body"
+    t.string   "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
