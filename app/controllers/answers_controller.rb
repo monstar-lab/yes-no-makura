32e72934a68cf6ed2ac1c7f2089012a96870866e
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2017-02-25 20:56:57 +0900

    :cop: Use Strong Parameters

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,17 +1,24 @@
 class AnswersController < ApplicationController
   # GET /answers/new
   def new
-    @cookie = params[:cookie_key]
+    @cookie = answer_params[:cookie_key]
     redirect_to action: :new, cookie_key: SecureRandom.uuid unless @cookie
   end
 
   # POST /answers
   def create
     question   = Question.find_by(state: :open)
-    cookie     = params[:cookie_key]
+    cookie     = answer_params[:cookie_key]
     attributes = { question: question, cookie_key: cookie }
-    results    = { yes: params[:yes] }
+    results    = { yes: answer_params[:yes] }
     Answer.find_or_initialize_by(attributes).update(results) if question
     redirect_to action: :new, cookie_key: cookie
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def answer_params
+    params.permit(:cookie_key, :yes)
   end
 end
