2d8c9ce1eb0417f008289bf6665f72ec33571f62
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2017-02-27 16:30:22 +0900

    :shell: rails destroy scaffold number_of_participants

--- db/migrate/20161012020613_create_number_of_participants.rb
+++
@@ -1,9 +0,0 @@
-class CreateNumberOfParticipants < ActiveRecord::Migration[5.0]
-  def change
-    create_table :number_of_participants do |t|
-      t.integer :number
-
-      t.timestamps
-    end
-  end
-end
