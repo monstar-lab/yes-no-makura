a00bed514ad8ac6e4029885440b898bed532f038
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2017-02-27 18:48:48 +0900

    :recycle: Fix some style

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,22 +9,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( animate.css )
-Rails.application.config.assets.precompile += %w( icomoon.css )
-Rails.application.config.assets.precompile += %w( bootstrap.css )
-Rails.application.config.assets.precompile += %w( magnific-popup.css )
-Rails.application.config.assets.precompile += %w( style.css )
-Rails.application.config.assets.precompile += %w( fonts.css )
-Rails.application.config.assets.precompile += %w( priority.css )
-Rails.application.config.assets.precompile += %w( frame.css )
-Rails.application.config.assets.precompile += %w( priority.css )
-
-Rails.application.config.assets.precompile += %w( modernizr-2.6.2.min.js )
-Rails.application.config.assets.precompile += %w( jquery.min.js )
-Rails.application.config.assets.precompile += %w( jquery.easing.1.3.js)
-Rails.application.config.assets.precompile += %w( bootstrap.min.js )
-Rails.application.config.assets.precompile += %w( jquery.waypoints.min.js )
-Rails.application.config.assets.precompile += %w( jquery.countTo.js )
-Rails.application.config.assets.precompile += %w( jquery.magnific-popup.min.js )
-Rails.application.config.assets.precompile += %w( magnific-popup-options.js )
-Rails.application.config.assets.precompile += %w( main.js )
