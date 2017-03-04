fe298aaa01f6f7d801a1db18af4a97e863c3cb67
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2017-03-04 20:27:52 +0900

    :shell: rubocop -a --only Lint/UnusedBlockArgument,Style/AndOr,Style/DefWithParentheses,Style/EmptyLineBetweenDefs,Style/EmptyLines,Style/EmptyLinesAroundAccessModifier,Style/EmptyLinesAroundBlockBody,Style/EmptyLinesAroundClassBody,Style/EmptyLinesAroundMethodBody,Style/EmptyLinesAroundModuleBody,Style/ExtraSpacing,Style/HashSyntax,Style/IndentationConsistency,Style/IndentationWidth,Style/MethodCallWithoutArgsParentheses,Style/MethodDefParentheses,Style/MultilineOperationIndentation,Style/SpaceAfterColon,Style/SpaceAfterComma,Style/SpaceAfterNot,Style/SpaceAfterSemicolon,Style/SpaceAroundEqualsInParameterDefault,Style/SpaceAroundOperators,Style/SpaceBeforeSemicolon,Style/SpaceInsideHashLiteralBraces,Style/StringLiterals,Style/TrailingBlankLines,Style/TrailingWhitespace

--- test/controllers/answers_controller_test.rb
+++ test/controllers/answers_controller_test.rb
@@ -5,40 +5,40 @@ class AnswersControllerTest < ActionDispatch::IntegrationTest
     @answer = answers(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get answers_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_answer_url
     assert_response :success
   end
 
-  test "should create answer" do
+  test 'should create answer' do
     assert_difference('Answer.count') do
-      post answers_url, params: { answer: {  } }
+      post answers_url, params: { answer: {} }
     end
 
     assert_redirected_to answer_url(Answer.last)
   end
 
-  test "should show answer" do
+  test 'should show answer' do
     get answer_url(@answer)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_answer_url(@answer)
     assert_response :success
   end
 
-  test "should update answer" do
-    patch answer_url(@answer), params: { answer: {  } }
+  test 'should update answer' do
+    patch answer_url(@answer), params: { answer: {} }
     assert_redirected_to answer_url(@answer)
   end
 
-  test "should destroy answer" do
+  test 'should destroy answer' do
     assert_difference('Answer.count', -1) do
       delete answer_url(@answer)
     end
