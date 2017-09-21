eec4225fd15278a79a575663943366b3645cda31
宮下 隼 <Hayato@miyashitashun-no-MacBook-Pro.local>
2017-09-21 11:25:38 +0900

    somefile unstaging

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 5.times do |n|
   body = "質問-#{n+1} "
   state = 'init'
