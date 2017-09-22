9a38d1e6dc16ccdcdc6aa28d6ae9b3f5d1e772d5
pgkuroneru <pgkuroneru@users.noreply.github.com>
2017-09-22 10:29:21 +0900

    (#88) Add controller test

---
+++ test/controllers/answers_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class AnswersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @cookie = SecureRandom.uuid
+    @answer = answers(:one)
+  end
+
+  test 'should get index' do
+    get root_url, params: { cookie_key: @cookie }
+    assert_response :success
+  end
+
+  test 'should get new answer' do
+    get root_url, params: { cookie_key: @cookie }
+    assert_response :success
+  end
+
+  test 'should destroy answer' do
+    assert_difference('Answer.count', -1) do
+      delete question_url(@answer)
+    end
+
+    assert_redirected_to questions_url
+  end
+end
---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class ApplicationControllerTest < ActionDispatch::IntegrationTest
+  setup do
+  end
+
+  test 'should get admin' do
+    get admin_url
+    assert_response :success
+  end
+end
