7ea86444abe8b68910bff383f5a05eb77b76f9d2
宮下 隼 <Hayato@moyasin.local>
2017-09-22 15:18:29 +0900

    refactoring answers.js

--- app/assets/javascripts/channels/answers.js
+++ app/assets/javascripts/channels/answers.js
@@ -3,15 +3,11 @@
         connected: () => {},
         disconnected: () => {},
         received: data => {
-            const count = data;
-            if (location.pathname === '/questions/'+ data['current_id'] + '/result') {
-                console.log("yesCount:" + count['yes_count']);
-                console.log("answersCount:" + count['answers_count']);
-                console.log("currentId:" + count['current_id']);
-                const answersCountEle = document.getElementById("answers-count");
-                const yesCountEle = document.getElementById("yes-count");
-                answersCountEle.textContent = "/" + count['answers_count'];
-                yesCountEle.textContent = "" + count['yes_count'];
+            if (location.pathname === `/questions/${data['current_id']}/result`) {
+                const answersCountEle = document.getElementById('answers-count');
+                const yesCountEle = document.getElementById('yes-count');
+                answersCountEle.textContent = '/' + data['answers_count'];
+                yesCountEle.textContent = data['yes_count'].toString();
             }
         }
     });
